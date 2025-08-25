0cf4d98fdc2b4fd7981993a169752214168f18e9
BrunoPoiano <bruno.poiano@proton.me>
2025-08-25 09:20:24 -0300

    Configure default axios instance without params

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-	baseURL: 'http://localhost:8247',
-	timeout: 1000,
+	// Used for development
+	// baseURL: 'http://localhost:8247',
+	// timeout: 1000,
 });
